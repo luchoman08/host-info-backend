d8d764bae18a98575f0c97e0d85e5b07d83c8096
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-14 17:00:35 -0500

    feat: main router is now using the api route

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
-	"fmt"
+	/*"fmt"
 	"github.com/luchoman08/ssllabs"
-	"./app"
+	"./app"*/
+	"github.com/go-chi/chi"
+	"github.com/luchoman08/ssllabs"
+	"net/http"
+	"./api"
 )
 
+type AppContext struct {
+	SsllabsClli ssllabs.Client
+}
+func (users *AppContext) showHandler(w http.ResponseWriter, r *http.Request) {
+	//now you can use users.db
+}
+func (users *AppContext) addHandler(w http.ResponseWriter, r *http.Request) {
+	//now you can use users.db
+}
 func main() {
-	c, _ := ssllabs.NewClient()
+	var client, _ = ssllabs.NewClient();
+	appContext := &AppContext{SsllabsClli: *client}
+	r := chi.NewRouter()
+	r.Route("/api/v1/", func(r chi.Router) {
+		r.Mount("/", api.Routes())
+	})
+	r.HandleFunc("/api/v1", appContext.showHandler)
+	http.ListenAndServe(":3000", r)
+	/*
 	var domainInfo, _ = app.ExtractDomainInfo("https://google.com/", c)
-	fmt.Println(domainInfo)
+	fmt.Println(domainInfo)*/
 }

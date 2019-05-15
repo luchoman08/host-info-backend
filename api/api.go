80137425be6fcebcc0f7cd4ebbf7381e2986ae3c
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-14 23:38:28 -0500

    refact: renaming bad named api functions

--- api/api.go
+++ api/api.go
@@ -1,32 +1,32 @@
 package api
 
 import (
+	"../app"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
-	"net/http"
-	"../app"
 	"github.com/luchoman08/ssllabs"
+	"net/http"
 )
 
 func Routes() *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/analyze", GetATodo)
+	router.Get("/analyze", GetServerInfo)
 	return router
 }
 
-func GetATodo(w http.ResponseWriter, r *http.Request) {
-	var client, _ = ssllabs.NewClient();
+func GetServerInfo(w http.ResponseWriter, r *http.Request) {
+	var client, _ = ssllabs.NewClient()
 	route := r.URL.Query().Get("host")
 	if route == "" {
 		http.Error(w, http.StatusText(400), 400)
 		render.JSON(w, r, app.ErrHostCannotBeEmpty.Error())
 		return
 	}
-	var domainInfo, err = app.ExtractDomainInfo(route, client )
+	var domainInfo, err = app.ExtractDomainInfo(route, client)
 	if err != nil {
 		http.Error(w, http.StatusText(400), 400)
 		render.JSON(w, r, err.Error())
 		return
 	}
-	render.JSON(w, r, domainInfo) // A chi router helper for serializing and returning json
+	render.JSON(w, r, domainInfo)
 }

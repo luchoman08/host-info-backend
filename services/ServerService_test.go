ffa437a3fe0ea1fbfd2e40ba84742e46d09bc054
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-18 19:30:38 -0500

    tests: added test for get minor ssl grade of servers in services functions

--- services/ServerService_test.go
+++ services/ServerService_test.go
@@ -1,1 +1,16 @@
-package repositories
+package services
+
+import (
+	"testing"
+	"../models"
+)
+
+func TestGetMinorSSLGrade(t *testing.T) {
+	serverService := ServerService{}
+	minorGrade := "C-"
+	servers := []models.ServerModel{{SslGrade:"C+"}, {SslGrade:"A"}, {SslGrade:"C"}, {SslGrade:minorGrade}}
+	minorGradeResult := serverService.GetMinorSSLGrade(servers)
+	if minorGradeResult != minorGrade {
+		t.Errorf("Minor grade: %s, given: %s", minorGrade, minorGradeResult)
+	}
+}
